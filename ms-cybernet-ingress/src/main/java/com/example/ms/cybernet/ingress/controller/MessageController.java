5fea1691440960d81c133782436a4eca553ee3fb
aydinibrahimov <aydininf@gmail.com>
2024-07-07 23:33:41 +0400

    change

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/MessageController.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/MessageController.java
@@ -1,17 +1,20 @@
 package com.example.ms.cybernet.ingress.controller;
 
+import com.example.ms.cybernet.ingress.model.request.PaymentRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("v1/payments")
 public class PaymentController {
 
     @GetMapping
-    @ResponseStatu("")
-    public void createPayment(String message) {
+
+    public void createPayment(PaymentRequest paymentRequest) {
         return "dfcgvhbj";
 
     }
