d567f0d63f2e123cc111c43a1e183e985c5e020b
aydinibrahimov <aydininf@gmail.com>
2024-07-09 22:05:11 +0400

    implemention of filter method of Stream

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/MyStream.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/MyStream.java
@@ -2,16 +2,34 @@ package com.example.ms.cybernet.ingress.controller;
 
 import org.apache.commons.lang3.stream.Streams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class MyStream {
 
     public static void main(String[] args) {
+
         List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
-        long a = strList.stream().map(x -> !x.isEmpty()).count();
-        long b = strList.stream().map(Streams::nonNull).count();
+
+        long a = strList.stream().filter(x -> !x.isEmpty()).count();
+
+        int b=myFilter(strList, x->!x.isEmpty()).size();
+
         System.out.println("My outputs " + a + " " + b);
+    }
+
+
+
+    public static <T> List<T> myFilter(List<T> list, Predicate<T> predicate) {
+        List<T> result = new ArrayList<>();
+        for (T element : list) {
+            if (predicate.test(element)) {
+                result.add(element);
+            }
+        }
+        return result;
     }
 }
