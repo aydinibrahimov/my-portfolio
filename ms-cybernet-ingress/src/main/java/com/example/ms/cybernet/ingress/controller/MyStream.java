1efc2067952aeabdfeb8797b19cc4a2174cf1661
aydinibrahimov <aydininf@gmail.com>
2024-07-09 21:48:02 +0400

    change

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/MyStream.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/MyStream.java
@@ -1,8 +1,17 @@
 package com.example.ms.cybernet.ingress.controller;
 
+import org.apache.commons.lang3.stream.Streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class MyStream {
 
     public static void main(String[] args) {
-        System.out.println("rdfgh");
+        List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
+        long a = strList.stream().map(x -> !x.isEmpty()).count();
+        long b = strList.stream().map(Streams::nonNull).count();
+        System.out.println("My outputs " + a + " " + b);
     }
 }
