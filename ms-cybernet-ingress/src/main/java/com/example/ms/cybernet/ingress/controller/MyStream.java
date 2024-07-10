71ecb3c6b43a80150b8f19579bbcf8fa5adecbe4
aydinibrahimov <aydininf@gmail.com>
2024-07-10 22:38:55 +0400

    implemention of filter method of Stream

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/MyStream.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/MyStream.java
@@ -5,18 +5,34 @@ import org.apache.commons.lang3.stream.Streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MyStream {
 
     public static void main(String[] args) {
 
+
+
+
+        List<String> list = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");
+
+        List<String> capitalCountryNames= list.stream().map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(list+"\n"+capitalCountryNames);
+
+
+
+
+
         List<String> strList = Arrays.asList("azn", "", "eur", "", "tl", "lari");
 
         int b = myFilter(strList, x -> !x.isEmpty()).size(); // 1
 
         long a = strList.stream().filter(x -> !x.isEmpty()).count(); // 2
+
+
 
     }
 
@@ -31,4 +47,5 @@ public class MyStream {
         return result;
     }
 
+    int a= Function<String,int>->
 }
