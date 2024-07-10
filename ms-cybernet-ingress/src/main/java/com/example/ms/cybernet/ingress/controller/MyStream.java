049418472c90bbea6d25614fef9fadcdd01e051e
aydinibrahimov <aydininf@gmail.com>
2024-07-10 23:12:02 +0400

    Summary Statistics

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/MyStream.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/MyStream.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.stream.Streams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -13,17 +14,24 @@ import java.util.stream.Stream;
 public class MyStream {
 
     public static void main(String[] args) {
+
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+    IntSummaryStatistics primesStatistics=    primes.stream().mapToInt(x->x)
+                        .summaryStatistics();
+        System.out.println(primesStatistics.getAverage());
+
+
         List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
-        List<Integer> distinctNumbers=   numbers.stream()
-                .map(x->x*5)
+        List<Integer> distinctNumbers = numbers.stream()
                 .distinct()
+                .map(x -> x * 5)
                 .collect(Collectors.toList());
         System.out.println(distinctNumbers);
 
 
         List<String> list = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.", "Canada");
 
-       String capitalCountryNames = list.stream()
+        String capitalCountryNames = list.stream()
                 .map(String::toUpperCase)
                 .collect(Collectors.joining(", "));
 
