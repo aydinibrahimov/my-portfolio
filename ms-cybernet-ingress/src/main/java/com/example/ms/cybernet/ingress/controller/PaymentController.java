f10dd0bf49b8131e94b194fe60794b21f782678f
aydinibrahimov <aydininf@gmail.com>
2024-07-14 13:00:39 +0400

    Microservices

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/PaymentController.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.example.ms.cybernet.ingress.model.request.PaymentRequest;
 import com.example.ms.cybernet.ingress.service.abstraction.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +13,7 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
-    @GetMapping
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createPayment(PaymentRequest paymentRequest) {
         paymentService.createPayment(paymentRequest);
