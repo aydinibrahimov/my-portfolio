e689284438256708bf421aadbd3e60d22d9d8646
aydinibrahimov <aydininf@gmail.com>
2024-07-28 21:37:51 +0400

    Check if an integer is even or odd bit level

---
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
@@ -0,0 +1,16 @@
+package com.example.ms.cybernet.ingress.lesson_15.entity;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int n = 5;
+
+        if ((n & 1) != 0) {
+            System.out.println(n + " is odd");
+        }
+        else {
+            System.out.println(n + " is even");
+        }
+    }
+}
