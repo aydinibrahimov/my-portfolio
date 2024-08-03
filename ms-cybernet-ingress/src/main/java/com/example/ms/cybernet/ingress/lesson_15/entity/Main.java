6c6fd8b5bc5c683bc0428dab2bb6c13a82b7ed3a
aydinibrahimov <aydininf@gmail.com>
2024-08-03 11:27:34 +0400

    @Transactional(propagation = Propagation.REQUIRES_NEW)

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
@@ -3,8 +3,19 @@ package com.example.ms.cybernet.ingress.lesson_15.entity;
 public class Main {
 
     public static void main(String[] args) {
-        String a0="1st value";
-        String a1="2nd value";
-        System.out.println(a1==a0);
+
+        Post p = new Post();
+        Post q = new Post();
+        p.setId(1L);
+        q.setId(1L);
+
+
+        System.out.println(p.getId() == q.getId());
+        System.out.println(p.getId().equals(q.getId()));
+        String a0 = "1st value";
+        String a1 = "1st  value";
+        System.out.println(a1.equals(a0));
+        int a = 5, b = 5;
+        System.out.println(a == b);
     }
 }
