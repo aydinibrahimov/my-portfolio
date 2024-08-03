966ed87963bf2da348a181e4fa7f9887bec7aa1e
aydinibrahimov <aydininf@gmail.com>
2024-08-03 11:16:50 +0400

    @Transactional(propagation = Propagation.REQUIRES_NEW)

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
@@ -3,5 +3,8 @@ package com.example.ms.cybernet.ingress.lesson_15.entity;
 public class Main {
 
     public static void main(String[] args) {
+        String a0="1st value";
+        String a1="2nd value";
+        System.out.println(a1==a0);
     }
 }
