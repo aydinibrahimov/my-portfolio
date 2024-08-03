283f2310c3d37ca9f12d3bc8350e2bf8f8d619b2
aydinibrahimov <aydininf@gmail.com>
2024-08-03 11:15:16 +0400

    @Transactional(propagation = Propagation.REQUIRES_NEW)

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
@@ -2,15 +2,6 @@ package com.example.ms.cybernet.ingress.lesson_15.entity;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        int n = 5;
-
-        if ((n & 1) != 0) {
-            System.out.println(n + " is odd");
-        }
-        else {
-            System.out.println(n + " is even");
-        }
+    public static void main(String[] args) {
     }
 }
