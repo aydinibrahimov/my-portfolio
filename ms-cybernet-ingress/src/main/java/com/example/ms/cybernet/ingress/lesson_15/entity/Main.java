f0b3de2a5313ff05429bda647f11d43f9addaceb
aydinibrahimov <aydininf@gmail.com>
2024-08-04 16:31:40 +0400

    @Transactional(propagation = Propagation.REQUIRES_NEW)

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
@@ -3,31 +3,16 @@ package com.example.ms.cybernet.ingress.lesson_15.entity;
 import java.util.Objects;
 
 public class Main {
+   static char cr;
 
     Long q=5L;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Main main = (Main) o;
-        return Objects.equals(q, main.q);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(q);
-    }
+
+
 
     public static void main(String[] args) {
-//        Object o1=new Object();
-//        Object o2=new Object();
-//        System.out.println(o2.equals(o1));
 
-
-        String str1 ="Hello";
-        String str2 = "Hello";
-        System.out.println(str1==str2);
 
     }
 }
