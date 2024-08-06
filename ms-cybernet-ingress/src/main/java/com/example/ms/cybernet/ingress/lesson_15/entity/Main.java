79032488a3dca094320a72bbf94eee771099220f
aydinibrahimov <aydininf@gmail.com>
2024-08-06 22:41:18 +0400

    @Transactional(propagation = Propagation.REQUIRES_NEW)

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
@@ -3,16 +3,26 @@ package com.example.ms.cybernet.ingress.lesson_15.entity;
 import java.util.Objects;
 
 public class Main {
-   static char cr;
-
-    Long q=5L;
-    Long p=5L;
 
 
+    //    reversing char array using two pointer technique
+    public static String reverseString(char[] value) {
+        int start = 0;
+        int end = value.length - 1;
+        char temp;
+        while (start < end) {
+            temp = value[start];
+            value[start] = value[end];
+            value[end] = temp;
 
+        }
+        return value.toString();
+    }
 
 
     public static void main(String[] args) {
+        char[] cr = {'k', 'e', 'l', 'e', 'm'};
+        System.out.println(reverseString(cr));
 
 
     }
