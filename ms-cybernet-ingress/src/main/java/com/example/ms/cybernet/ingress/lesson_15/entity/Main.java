dfc84e182676b8b8aa0a174defeaf82fdbddc72b
aydinibrahimov <aydininf@gmail.com>
2024-08-06 23:36:47 +0400

    @Transactional(propagation = Propagation.REQUIRES_NEW)

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
@@ -5,25 +5,37 @@ import java.util.Objects;
 public class Main {
 
 
-    //    reversing char array using two pointer technique
-    public static String reverseString(char[] value) {
-        int start = 0;
-        int end = value.length - 1;
-        char temp;
-        while (start < end) {
-            temp = value[start];
-            value[start] = value[end];
-            value[end] = temp;
-
-        }
-        return value.toString();
-    }
-
-
     public static void main(String[] args) {
         char[] cr = {'k', 'e', 'l', 'e', 'm'};
         System.out.println(reverseString(cr));
 
-
     }
+
+    public static ListNode middleNode(ListNode head) {
+
+        var slow=head;
+        var fast=head;
+
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
+    //    reversing char array using two pointer technique
+    public static String reverseString(char[] value) {
+        int start = 0;
+        int end = value.length - 1;
+
+        while (start < end) {
+            char temp = value[start];
+            value[start] = value[end];
+            value[end] = temp;
+            start++;
+            end--;
+        }
+        return String.valueOf(value);
+    }
+
 }
