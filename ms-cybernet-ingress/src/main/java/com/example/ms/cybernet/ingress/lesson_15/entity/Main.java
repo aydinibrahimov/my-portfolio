efb8d4ec5ef4bcbf5eb885df29dbc61220b33105
aydinibrahimov <aydininf@gmail.com>
2024-08-27 20:32:10 +0400

    @Transactional(propagation = Propagation.REQUIRES_NEW)

--- ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
+++ ms-cybernet-ingress/src/main/java/com/example/ms/cybernet/ingress/lesson_15/entity/Main.java
@@ -10,22 +10,72 @@ public class Main {
 
 
     public static void main(String[] args) {
-        fib(3);
+//        fib(3);
+    //    System.out.println(tribonacci(4));
+List<Integer> nums=List.of(4,6,10);
+
+        System.out.println(sumPossible(15,nums));
     }
 
 
+    //Sum Possible
+    public static boolean sumPossible(int amount , List<Integer> numbers){
+        return sumPossible(amount,numbers, new HashMap<>());
+    }
+
+    public static boolean sumPossible(int amount , List<Integer> numbers, HashMap<Integer,Boolean> memo){
+        if (amount==0) return true;
+        if(amount<0) return false;
+
+        if(memo.containsKey(amount)){
+            return memo.get(amount);
+        }
+
+        for(int num:numbers){
+            int sumAmount=amount-num;
+            if(sumPossible(sumAmount,numbers,memo)){
+                memo.put(amount,true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Tribonacci
+
+    public static int tribonacci(int n) {
+        return tribonacci(n, new HashMap<>());
+    }
+
+    public static int tribonacci(int n, HashMap<Integer, Integer> memo) {
+        if (n == 0 ) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        int result = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
+        memo.put(n, result);
+        return result;
+    }
+
+    // 1 fibonacci
     public static int fib(int n) {
-        return fib1(n, new HashMap<>());
+        return fib(n, new HashMap<>());
     }
 
-    public static int fib1(int n, HashMap<Integer, Integer> memo) {
+    public static int fib(int n, HashMap<Integer, Integer> memo) {
         if (n == 0 || n == 1) {
             return n;
         }
         if (memo.containsKey(n)) {
             return memo.get(n);
         }
-        int result = fib1(n - 1, memo) + fib1(n - 2, memo);
+        int result = fib(n - 1, memo) + fib(n - 2, memo);
         memo.put(n, result);
         return result;
     }
