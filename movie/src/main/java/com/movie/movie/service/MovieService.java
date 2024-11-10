1e3a251fa49522700acca5618beedf0c11c61bed
aydinibrahimov <aydininf@gmail.com>
2024-11-10 22:15:20 +0400

    Movie Service

--- movie/src/main/java/com/movie/movie/service/MovieService.java
+++ movie/src/main/java/com/movie/movie/service/MovieService.java
@@ -3,21 +3,46 @@ package com.movie.movie.service;
 import com.movie.movie.dto.MovieDto;
 import com.movie.movie.entity.Movie;
 import com.movie.movie.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final FileService fileService;
 
-    public MovieService(MovieRepository movieRepository) {
-        this.movieRepository = movieRepository;
+    @Value("${project.poster}")
+    private String path;
+
+    @Value("${base.url}")
+    private String baseUrl;
+
+
+    public MovieDto addMovie(MovieDto movieDto, MultipartFile file) {
+        var movie = movieRepository.save(buildMovie(movieDto));
+        return buildMovieDto(movie);
     }
 
     public MovieDto getMovie(Integer id) {
         var movie = movieRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Movie not found"));
         return buildMovieDto(movie);
+    }
+
+
+    public List<MovieDto> getMovie() {
+        var movieList = movieRepository.findAll();
+        var movieDtoList = movieList.stream()
+                .map(x -> buildMovieDto(x))
+                .collect(Collectors.toList());
+        return movieDtoList;
     }
 
 
