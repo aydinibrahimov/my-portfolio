c226256f1c1b3c677013f2748322465806a881de
aydinibrahimov <aydininf@gmail.com>
2024-12-01 00:07:24 +0400

    RabbitMQ using Docker

--- hibernate/src/main/java/com/hibernate/service/PaymentService.java
+++ hibernate/src/main/java/com/hibernate/service/PaymentService.java
@@ -1,7 +1,9 @@
 package com.hibernate.service;
 
 import com.hibernate.requests.CreatePaymentRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PaymentService {
     public void createPayment(CreatePaymentRequest paymentRequest) {
     }
